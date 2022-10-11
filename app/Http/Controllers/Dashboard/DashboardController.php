e70ce45b50a66498b32114f99baa58111077513d
markchovava <markchovava@gmail.com>
2022-10-11 12:50:23 +0200

    Working on Dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,17 +4,48 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Operation\Operation;
+use App\Models\Order\Order;
+use App\Models\Product\Product;
+use App\Models\Stock\Stock;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
    public function index(){
       // User Status
-      $auth_id = Auth::user()->id;
-      $operation_status = Operation::where('user_id', $auth_id)->first();
+      $user_id = Auth::id();
+      $operation_status = Operation::where('user_id', $user_id)->first();
       $data['operation_status'] = $operation_status;
-      
+      // Active Operators
+      $operation = Operation::where('status', 1)->get();
+      $data['operations'] = $operation;
+      //Stock
+      $stock = Stock::sum('quantity');
+      $data['stock'] = $stock;
+      // USD Sales
+      $items = DB::table('order_items')
+                ->whereDate('created_at', Carbon::today())
+                ->where('currency', 'USD')
+                ->get();
+      $data['usd_sales'] = $items->sum('product_total');
+      // ZWL Sales
+      $items = DB::table('order_items')
+                ->whereDate('created_at', Carbon::today())
+                ->where('currency', 'ZWL')
+                ->get();
+      $data['zwl_sales'] = $items->sum('product_total');
+      // Products
+      $products = Product::get();
+      $data['products'] = $products;
+      // Daily Orders
+      $orders = Order::whereDate('created_at', Carbon::today())->get();
+      $data['orders'] = $orders;
+
+
       return view('backend.dashboard.index', $data);
    }
+
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -15,14 +15,14 @@
 				<div class="card-body">
 					<!-- BEGIN title -->
 					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">POINT OF SALE</span>
+						<span class="flex-grow-1"><a href="{{ route('admin.pos') }}">POINT OF SALE</a></span>
 						<a href="{{ route('admin.pos') }}" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
 					</div>
 					<!-- END title -->
 					<!-- BEGIN stat-lg -->
 					<div class="row align-items-center mb-2">
 						<div class="col-7">
-							<h3 class="mb-0">33</h3>
+							<h3 class="mb-0">{{ count($operations) }}</h3>
 						</div>
 						<div class="col-5">
 						</div>
@@ -30,7 +30,7 @@
 					<!-- END stat-lg -->
 					<!-- BEGIN stat-sm -->
 					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="far fa-user fa-fw me-1"></i> 33 Active at the moment.
+						<i class="far fa-user fa-fw me-1"></i> {{ count($operations) }} Active at the moment.
 					</div>
 					<!-- END stat-sm -->
 				</div>
@@ -57,14 +57,18 @@
 				<div class="card-body">
 					<!-- BEGIN title -->
 					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">SALES</span>
+						<span class="flex-grow-1"><a href="{{ route('admin.sale.index') }}">USD DAILY SALES</a></span>
 						<a href="{{ route('admin.sale.index') }}" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
 					</div>
 					<!-- END title -->
 					<!-- BEGIN stat-lg -->
 					<div class="row align-items-center mb-2">
 						<div class="col-7">
-							<h3 class="mb-0">$35.2K</h3>
+							@php
+								$usd_calculate = (int)$usd_sales /100;
+								$usd_decimal = number_format($usd_calculate, 2, '.', '');
+							@endphp
+							<h3 class="mb-0">${{ $usd_decimal }}</h3>
 						</div>
 						<div class="col-5">
 							
@@ -73,92 +77,7 @@
 					<!-- END stat-lg -->
 					<!-- BEGIN stat-sm -->
 					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="fa fa-chevron-up fa-fw me-1"></i> 20.4% more than last week<br />
-					</div>
-					<!-- END stat-sm -->
-				</div>
-				<!-- END card-body -->
-				
-				<!-- BEGIN card-arrow -->
-				<div class="card-arrow">
-					<div class="card-arrow-top-left"></div>
-					<div class="card-arrow-top-right"></div>
-					<div class="card-arrow-bottom-left"></div>
-					<div class="card-arrow-bottom-right"></div>
-				</div>
-				<!-- END card-arrow -->
-			</div>
-			<!-- END card -->
-		</div>
-		<!-- END col-3 -->
-		
-		<!-- BEGIN col-3 -->
-		<div class="col-xl-3 col-lg-6">
-			<!-- BEGIN card -->
-			<div class="card mb-3">
-				<!-- BEGIN card-body -->
-				<div class="card-body">
-					<!-- BEGIN title -->
-					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">STOCK</span>
-						<a href="{{ route('admin.stock.index') }}" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-					</div>
-					<!-- END title -->
-					<!-- BEGIN stat-lg -->
-					<div class="row align-items-center mb-2">
-						<div class="col-7">
-							<h3 class="mb-0">4,490</h3>
-						</div>
-						<div class="col-5">
-							
-						</div>
-					</div>
-					<!-- END stat-lg -->
-					<!-- BEGIN stat-sm -->
-					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="fas fa-lg fa-fw me-2 fa-info"></i> 1212 more than last week
-					</div>
-					<!-- END stat-sm -->
-				</div>
-				<!-- END card-body -->
-				
-				<!-- BEGIN card-arrow -->
-				<div class="card-arrow">
-					<div class="card-arrow-top-left"></div>
-					<div class="card-arrow-top-right"></div>
-					<div class="card-arrow-bottom-left"></div>
-					<div class="card-arrow-bottom-right"></div>
-				</div>
-				<!-- END card-arrow -->
-			</div>
-			<!-- END card -->
-		</div>
-		<!-- END col-3 -->
-		
-		<!-- BEGIN col-3 -->
-		<div class="col-xl-3 col-lg-6">
-			<!-- BEGIN card -->
-			<div class="card mb-3">
-				<!-- BEGIN card-body -->
-				<div class="card-body">
-					<!-- BEGIN title -->
-					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">PRODUCTS</span>
-						<a href="{{ route('admin.product.index') }}" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-					</div>
-					<!-- END title -->
-					<!-- BEGIN stat-lg -->
-					<div class="row align-items-center mb-2">
-						<div class="col-7">
-							<h3 class="mb-0">4.5TB</h3>
-						</div>
-						<div class="col-5">
-						</div>
-					</div>
-					<!-- END stat-lg -->
-					<!-- BEGIN stat-sm -->
-					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="fa fa-chevron-up fa-fw me-1"></i> 5.3% more than last week<br />
+						<i class="fa fa-chevron-up fa-fw me-1"></i> ${{ $usd_decimal }} today.<br />
 					</div>
 					<!-- END stat-sm -->
 				</div>
@@ -185,14 +104,146 @@
 				<div class="card-body">
 					<!-- BEGIN title -->
 					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">ORDERS</span>
+						<span class="flex-grow-1"><a href="{{ route('admin.sale.index') }}">ZWL DAILY SALES</a></span>
+						<a href="{{ route('admin.sale.index') }}" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
+					</div>
+					<!-- END title -->
+					<!-- BEGIN stat-lg -->
+					<div class="row align-items-center mb-2">
+						<div class="col-7">
+							@php
+								$zwl_calculate = (int)$zwl_sales /100;
+								$zwl_decimal = number_format($zwl_calculate, 2, '.', '');
+							@endphp
+							<h3 class="mb-0">${{ $zwl_decimal }}</h3>
+						</div>
+						<div class="col-5">
+							
+						</div>
+					</div>
+					<!-- END stat-lg -->
+					<!-- BEGIN stat-sm -->
+					<div class="small text-white text-opacity-50 text-truncate">
+						<i class="fa fa-chevron-up fa-fw me-1"></i> ${{ $zwl_decimal }} today.<br />
+					</div>
+					<!-- END stat-sm -->
+				</div>
+				<!-- END card-body -->
+				
+				<!-- BEGIN card-arrow -->
+				<div class="card-arrow">
+					<div class="card-arrow-top-left"></div>
+					<div class="card-arrow-top-right"></div>
+					<div class="card-arrow-bottom-left"></div>
+					<div class="card-arrow-bottom-right"></div>
+				</div>
+				<!-- END card-arrow -->
+			</div>
+			<!-- END card -->
+		</div>
+		<!-- END col-3 -->
+		
+		<!-- BEGIN col-3 -->
+		<div class="col-xl-3 col-lg-6">
+			<!-- BEGIN card -->
+			<div class="card mb-3">
+				<!-- BEGIN card-body -->
+				<div class="card-body">
+					<!-- BEGIN title -->
+					<div class="d-flex fw-bold small mb-3">
+						<span class="flex-grow-1"><a href="{{ route('admin.stock.index') }}">STOCK</a></span>
+						<a href="{{ route('admin.stock.index') }}" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
+					</div>
+					<!-- END title -->
+					<!-- BEGIN stat-lg -->
+					<div class="row align-items-center mb-2">
+						<div class="col-7">
+							<h3 class="mb-0">{{ $stock }}</h3>
+						</div>
+						<div class="col-5">
+							
+						</div>
+					</div>
+					<!-- END stat-lg -->
+					<!-- BEGIN stat-sm -->
+					<div class="small text-white text-opacity-50 text-truncate">
+						<i class="fas fa-lg fa-fw me-2 fa-info"></i> {{ $stock }} is the current stock.
+					</div>
+					<!-- END stat-sm -->
+				</div>
+				<!-- END card-body -->
+				
+				<!-- BEGIN card-arrow -->
+				<div class="card-arrow">
+					<div class="card-arrow-top-left"></div>
+					<div class="card-arrow-top-right"></div>
+					<div class="card-arrow-bottom-left"></div>
+					<div class="card-arrow-bottom-right"></div>
+				</div>
+				<!-- END card-arrow -->
+			</div>
+			<!-- END card -->
+		</div>
+		<!-- END col-3 -->
+		
+		<!-- BEGIN col-3 -->
+		<div class="col-xl-3 col-lg-6">
+			<!-- BEGIN card -->
+			<div class="card mb-3">
+				<!-- BEGIN card-body -->
+				<div class="card-body">
+					<!-- BEGIN title -->
+					<div class="d-flex fw-bold small mb-3">
+						<span class="flex-grow-1"><a href="{{ route('admin.product.index') }}"> PRODUCTS </a></span>
+						<a href="{{ route('admin.product.index') }}" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
+					</div>
+					<!-- END title -->
+					<!-- BEGIN stat-lg -->
+					<div class="row align-items-center mb-2">
+						<div class="col-7">
+							<h3 class="mb-0">{{ count($products) }}</h3>
+						</div>
+						<div class="col-5">
+						</div>
+					</div>
+					<!-- END stat-lg -->
+					<!-- BEGIN stat-sm -->
+					<div class="small text-white text-opacity-50 text-truncate">
+						<i class="fa fa-chevron-up fa-fw me-1"></i> {{ count($products) }} product types in store.<br />
+					</div>
+					<!-- END stat-sm -->
+				</div>
+				<!-- END card-body -->
+				
+				<!-- BEGIN card-arrow -->
+				<div class="card-arrow">
+					<div class="card-arrow-top-left"></div>
+					<div class="card-arrow-top-right"></div>
+					<div class="card-arrow-bottom-left"></div>
+					<div class="card-arrow-bottom-right"></div>
+				</div>
+				<!-- END card-arrow -->
+			</div>
+			<!-- END card -->
+		</div>
+		<!-- END col-3 -->
+
+		<!-- BEGIN col-3 -->
+		<div class="col-xl-3 col-lg-6">
+			<!-- BEGIN card -->
+			<div class="card mb-3">
+				<!-- BEGIN card-body -->
+				<div class="card-body">
+					<!-- BEGIN title -->
+					<div class="d-flex fw-bold small mb-3">
+						<span class="flex-grow-1"> <a href="{{ route('admin.order.index') }}">ORDERS </a></span>
 						<a href="{{ route('admin.order.index') }}" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
 					</div>
 					<!-- END title -->
 					<!-- BEGIN stat-lg -->
 					<div class="row align-items-center mb-2">
 						<div class="col-7">
-							<h3 class="mb-0">4.2m</h3>
+							<h3 class="mb-0">{{ count($orders) }}</h3>
 						</div>
 						<div class="col-5">
 							
@@ -201,7 +252,7 @@
 					<!-- END stat-lg -->
 					<!-- BEGIN stat-sm -->
 					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="fa fa-chevron-up fa-fw me-1"></i> 33.3% more than last week<br />
+						<i class="fa fa-chevron-up fa-fw me-1"></i> {{ count($orders) }} more than last week. <br />
 					</div>
 					<!-- END stat-sm -->
 				</div>
@@ -220,92 +271,7 @@
 		</div>
 		<!-- END col-3 -->
 		
-		<!-- BEGIN col-3 -->
-		<div class="col-xl-3 col-lg-6">
-			<!-- BEGIN card -->
-			<div class="card mb-3">
-				<!-- BEGIN card-body -->
-				<div class="card-body">
-					<!-- BEGIN title -->
-					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">CUSTOMERS</span>
-						<a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-					</div>
-					<!-- END title -->
-					<!-- BEGIN stat-lg -->
-					<div class="row align-items-center mb-2">
-						<div class="col-7">
-							<h3 class="mb-0">$35.2K</h3>
-						</div>
-						<div class="col-5">
-							
-						</div>
-					</div>
-					<!-- END stat-lg -->
-					<!-- BEGIN stat-sm -->
-					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="fa fa-chevron-up fa-fw me-1"></i> 20.4% more than last week<br />
-					</div>
-					<!-- END stat-sm -->
-				</div>
-				<!-- END card-body -->
-				
-				<!-- BEGIN card-arrow -->
-				<div class="card-arrow">
-					<div class="card-arrow-top-left"></div>
-					<div class="card-arrow-top-right"></div>
-					<div class="card-arrow-bottom-left"></div>
-					<div class="card-arrow-bottom-right"></div>
-				</div>
-				<!-- END card-arrow -->
-			</div>
-			<!-- END card -->
-		</div>
-		<!-- END col-3 -->
-		
-		<!-- BEGIN col-3 -->
-		<div class="col-xl-3 col-lg-6">
-			<!-- BEGIN card -->
-			<div class="card mb-3">
-				<!-- BEGIN card-body -->
-				<div class="card-body">
-					<!-- BEGIN title -->
-					<div class="d-flex fw-bold small mb-3">
-						<span class="flex-grow-1">OPERATIONS</span>
-						<a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-					</div>
-					<!-- END title -->
-					<!-- BEGIN stat-lg -->
-					<div class="row align-items-center mb-2">
-						<div class="col-7">
-							<h3 class="mb-0">4,490</h3>
-						</div>
-						<div class="col-5">
-						
-						</div>
-					</div>
-					<!-- END stat-lg -->
-					<!-- BEGIN stat-sm -->
-					<div class="small text-white text-opacity-50 text-truncate">
-						<i class="fa fa-chevron-up fa-fw me-1"></i> 59.5% more than last week<br />
-					</div>
-					<!-- END stat-sm -->
-				</div>
-				<!-- END card-body -->
-				
-				<!-- BEGIN card-arrow -->
-				<div class="card-arrow">
-					<div class="card-arrow-top-left"></div>
-					<div class="card-arrow-top-right"></div>
-					<div class="card-arrow-bottom-left"></div>
-					<div class="card-arrow-bottom-right"></div>
-				</div>
-				<!-- END card-arrow -->
-			</div>
-			<!-- END card -->
-		</div>
-		<!-- END col-3 -->
-	
+
 		
 	</div>
 	<!-- END row -->
